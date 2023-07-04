62b3053bdc65d13b6cdc395735b0e600ff87522e
Hindol19 <hindolbanerjee5@gmail.com>
2023-07-04 13:18:56 +0530

    Imported Shiba

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,50 @@
 import { useEffect } from "react";
-import * as THREE from "three";
 
-import "./App.css";
+import * as THREE from "three";
+// import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
+// import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+// import { VOXLoader } from 'three/examples/jsm/loaders/VOXLoader';
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+
+import SceneInit from "./lib/SceneInit";
 
 function App() {
   useEffect(() => {
-    const scene = new THREE.Scene();
+    const test = new SceneInit("myThreeJsCanvas");
+    test.initialize();
+    test.animate();
 
-    const camera = new THREE.PerspectiveCamera(
-      50,
-      window.innerWidth / window.innerHeight,
-      1,
-      1000
-    );
-    camera.position.z = 96;
+    // const boxGeometry = new THREE.BoxGeometry(8, 8, 8);
+    // const boxMaterial = new THREE.MeshNormalMaterial();
+    // const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+    // test.scene.add(boxMesh);
 
-    const canvas = document.getElementById("myCanvas");
-    const renderer = new THREE.WebGLRenderer({
-      canvas,
-      antialias: true,
+    let loadedModel;
+    const glftLoader = new GLTFLoader();
+    glftLoader.load("./assets/shiba/scene.gltf", (gltfScene) => {
+      loadedModel = gltfScene;
+      // console.log(loadedModel);
+
+      gltfScene.scene.rotation.y = Math.PI / 8;
+      gltfScene.scene.position.y = 3;
+      gltfScene.scene.scale.set(10, 10, 10);
+      test.scene.add(gltfScene.scene);
     });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-    ambientLight.castShadow = true;
-    scene.add(ambientLight);
-
-    const spotLight = new THREE.SpotLight(0xffffff, 1);
-    spotLight.castShadow = true;
-    spotLight.position.set(0, 64, 32);
-    scene.add(spotLight);
-
-    const boxGeometry = new THREE.BoxGeometry(16, 16, 16);
-    const boxMaterial = new THREE.MeshNormalMaterial();
-    const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-    scene.add(boxMesh);
 
     const animate = () => {
-      boxMesh.rotation.x += 0.01;
-      boxMesh.rotation.y += 0.01;
-      renderer.render(scene, camera);
-      window.requestAnimationFrame(animate);
+      if (loadedModel) {
+        // loadedModel.scene.rotation.x += 0.01;
+        loadedModel.scene.rotation.y += 0.01;
+        // loadedModel.scene.rotation.z += 0.01;
+      }
+      requestAnimationFrame(animate);
     };
     animate();
   }, []);
 
   return (
     <div>
-      <canvas id="myCanvas" />
+      <canvas id="myThreeJsCanvas" />
     </div>
   );
 }
